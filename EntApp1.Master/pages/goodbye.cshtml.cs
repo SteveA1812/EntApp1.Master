02f095f711d9b903f55a9a1cf89bbc35098256e6
SteveA1812 <46904949+SteveA1812@users.noreply.github.com>
2019-01-27 15:50:05 +0000

    .

---
+++ EntApp1.Master/pages/goodbye.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EntApp1.Master.pages
+{
+    public class goodbyeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
